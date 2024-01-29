22ebef57c739ba55bf2b79955bb2911d200f5dda
ashok <ashok03012000@gmail.com>
2024-01-29 16:27:49 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+document.getElementById("btn").addEventListener("click", (event) => {
+    console.log("working");
+    event.preventDefault();
+
+    // const amount = document.getElementById("amount").value;
+    // const desc = document.getElementById("desc").value;
+    // const option = document.getElementById("select").value;
+
+    // console.log(amount, desc, option);
+    console.log("working");
+});
+
+
